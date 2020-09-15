3c379ffb039cfc676f9366762e0f3b3b17805aa1
little <little500ml@gmail.com>
2020-09-15 10:09:21 +0900

    フレームの色をシャッフルに変更

--- Assets/CardMoveSample/Script/CardFrameColorSet.cs
+++ Assets/CardMoveSample/Script/CardFrameColorSet.cs
@@ -21,25 +21,41 @@ public class CardFrameColorSet : MonoBehaviour {
         Color[] ColorArray = { Color.red, Color.green, Color.blue, Color.cyan, Color.magenta, Color.yellow, Color.white, new Color(1.0f, 0.5f, 0.0f, 1.0f) };
         playerCard = GameObject.FindGameObjectsWithTag("PlayerCard");
         playerDest = GameObject.FindGameObjectsWithTag("PlayerDestination");
+        ColorArray = Shuffle(ColorArray, ColorArray.Length);
 
         // 自分のカード枠
         for (int i = 0; i < playerCard.Length; i++) {
+            var colorIndex = (i+1)%ColorArray.Length;
             var cardStr = "Card (" + i + ")";
-            var ColorIndex = Random.Range(0, ColorArray.Length);
             if (playerCard[i].transform.name == "Card") {
-                playerCard[i].GetComponent<Image>().color = ColorArray[ColorIndex];
-                playerDest[i].GetComponent<Image>().color = ColorArray[ColorIndex];
+                playerCard[i].GetComponent<Image>().color = ColorArray[colorIndex];
+                playerDest[i].GetComponent<Image>().color = ColorArray[colorIndex];
                 playerCard[i].transform.localPosition = new Vector3(defaultPosX + posX * i, playertPosY, 0);
                 playerDest[i].transform.localPosition = new Vector3(defaultPosX + posX * i, playertPosY, 0);
 
             }
             else if (playerCard[i].transform.name == cardStr) {
-                playerCard[i].GetComponent<Image>().color = ColorArray[ColorIndex];
-                playerDest[i].GetComponent<Image>().color = ColorArray[ColorIndex];
+                playerCard[i].GetComponent<Image>().color = ColorArray[colorIndex];
+                playerDest[i].GetComponent<Image>().color = ColorArray[colorIndex];
                 playerCard[i].transform.localPosition = new Vector3(defaultPosX + posX * i, playertPosY, 0);
                 playerDest[i].transform.localPosition = new Vector3(defaultPosX + posX * i, playertPosY, 0);
 
             }
         }
+    }
+
+    // シャッフル
+    Color[] Shuffle(Color[] color, int maxIndex)
+    {
+        while (maxIndex > 1) {
+            maxIndex--;
+            int randIndex = UnityEngine.Random.Range(0, maxIndex + 1);
+
+            // 入れ替え
+            Color temp = color[randIndex];
+            color[randIndex] = color[maxIndex];
+            color[maxIndex] = temp;
+        }
+        return color;
     }
 }
